e927ef2bd7f0d04541f3c7a297a15f6c97f4de42
danstruble <dbstruble@gmail.com>
2020-04-23 16:07:31 -0400

    Step 8 of lab 10

--- lab_10/server.js
+++ lab_10/server.js
@@ -28,21 +28,9 @@ app.use(express.static('public'));
 
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
-
-  // Your Fetch API call starts here
-  // Note that at no point do you "return" anything from this function -
-  // it instead handles returning data to your front end at line 34.
-    fetch(baseURL)
-      .then((r) => r.json())
-      .then((data) => {
-        console.log(data);
-        res.send({ data: data }); // here's where we return data to the front end
-      })
-      .catch((err) => {
-        console.log(err);
-        res.redirect('/error');
-      });
+  db.all("SELECT * FROM forms;", (err, rows) => {
+    res.json({'data':rows});
+  })
 }
 
 // Syntax change - we don't want to repeat ourselves,
